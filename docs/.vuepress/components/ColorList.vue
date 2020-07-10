476688512870e025c0db5ab3891d9a5a62c8a709
Enaah <mathis.bin@icloud.com>
2020-07-10 17:03:13 +0200

    fix/Color-Section
    - reenables the  "Color"-Section after it broke because of changes in the TailwindCss config (`tailwind.js`) after a versionupgrade (>= 1.4.6)

--- docs/.vuepress/components/ColorList.vue
+++ docs/.vuepress/components/ColorList.vue
@@ -2,7 +2,7 @@
     <div>
         <div v-for="(groupColors, groupKey) in groupedColors">
             <group-block group-name="color" :heading="groupKey">
-                <color-spread :colors="groupColors" />
+                <color-spread :colors="[groupKey, groupColors]" />
             </group-block>
         </div>
     </div>
@@ -14,14 +14,16 @@ import _ from 'lodash'
 export default {
     props: ['colors'],
     computed: {
-        colorKeys: function () {
-            return _.keys(this.colors)
-        },
         groupedColors: function () {
-            return _.groupBy(this.colorKeys, function (key) {
-                console.log(key)
-                return key.split('-')[0]
-            })
+            for (let element in this.colors) {
+                if (typeof this.colors[`${element}`] !== 'object') {
+                    let key = element
+                    let object = {}
+                    object[`${element}`] = this.colors[`${element}`]
+                    this.colors[`${element}`] = object
+                }
+            }
+            return this.colors
         },
     },
 }
--- docs/.vuepress/components/ColorSpread.vue
+++ docs/.vuepress/components/ColorSpread.vue
@@ -1,14 +1,22 @@
 <template>
     <table class="table table-fixed w-full">
         <tr>
-            <td v-for="color in colors" :class="'bg-' + color" class="h-16"></td>
+            <td
+                v-for="(color, index) in colorValues"
+                :class="`bg${colorClass}-${index}`"
+                class="h-16"
+            ></td>
         </tr>
         <tr>
-            <td v-for="color in colors" class="align-top text-center">
+            <td
+                v-for="(color, index) in colorValues"
+                class="align-top text-center"
+            >
                 <span
                     class="inline-block whitespace-no-wrap"
                     style="writing-mode: tb-rl;"
-                >{{ color }}</span>
+                    >{{ index }}</span
+                >
             </td>
         </tr>
     </table>
@@ -17,5 +25,16 @@
 <script>
 export default {
     props: ['colors'],
+    computed: {
+        colorValues: function () {
+            return this.colors[1]
+        },
+        colorClass: function () {
+            let colorName = this.colors[0]
+            return this.colors[1][`${colorName}`] !== undefined
+                ? ''
+                : `-${colorName}`
+        },
+    },
 }
 </script>
