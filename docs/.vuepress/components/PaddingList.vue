289b24f0eaec89a4db88a100cbabfba1d5afdb9a
Enaah <mathis.bin@icloud.com>
2020-08-12 18:01:59 +0200

    fix/Padding-Section
    - fixes the "padding" section

--- docs/.vuepress/components/PaddingList.vue
+++ docs/.vuepress/components/PaddingList.vue
@@ -1,9 +1,14 @@
 <template>
     <table class="table w-full">
         <tr v-for="(value, key) in padding">
-            <td class="px-5 py-3 whitespace-no-wrap" style="width: 1px;">{{key}} ({{value}})</td>
+            <td class="px-5 py-3 whitespace-no-wrap" style="width: 1px;">
+                {{ key }} ({{ value }})
+            </td>
             <td class="px-5 py-3">
-                <div :class="'pl-' + key" class="h-5 bg-grey inline-block"></div>
+                <div
+                    :class="'pl-' + key"
+                    class="h-5 bg-grey inline-block bg-black"
+                ></div>
             </td>
         </tr>
     </table>
